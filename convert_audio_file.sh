d0580f9e5a452815572f7a3750df5c95c4ae920d
Luca Ceragioli <36365955+VirtuContraFurore@users.noreply.github.com>
2023-04-03 23:13:37 +0200

    Update convert_audio_file.sh

--- convert_audio_file.sh
+++ convert_audio_file.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Usage: ./convert_audio_file.sh ./input_file.mp3
+
 CHANNELS=1
 SAMPLING_RATE=44100
 ffmpeg -i $1 -flags +bitexact -map_metadata -1 -ar $SAMPLING_RATE -ac $CHANNELS output.wav
