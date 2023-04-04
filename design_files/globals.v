1bc74fb9b608ab5d2b3215b70665943307d72c01
Luca Ceragioli <ceragioli_luca@libero.it>
2023-04-05 01:25:50 +0200

    Added a comment

--- design_files/globals.v
+++ design_files/globals.v
@@ -5,6 +5,8 @@
 /* Uncomment ignored config */
 //`define DEBUG_SPI_MASTER
 //`define DEBUG_SDCARD_READER
+
+/* Main clock is 200 MHz -> DIV8 = 25 MHz */
 `define SDCARD_MISSION_MODE_PSCLR SPI_PSCLR_DIV8
 `define STARTUP_PRESCALER         SPI_PSCLR_DIV1024
 
