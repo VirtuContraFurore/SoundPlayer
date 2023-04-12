fa7cfc9082c0d5efe7e70e3ca341e6e48671dbf2
Luca Ceragioli <ceragioli_luca@libero.it>
2023-04-12 22:19:27 +0200

    Added costant to handle buffer wait states during reading

--- design_files/buffer_consts.v
+++ design_files/buffer_consts.v
@@ -9,3 +9,6 @@ parameter BUFFER_SIZE_BYTES = (RAM_SIZE_BYTES/2);
 
 parameter RAM_ADDR_BITS    = $clog2(RAM_SIZE_BYTES);
 parameter BUFFER_ADDR_BITS = $clog2(BUFFER_SIZE_BYTES);
+
+parameter RAM_READ_WAIT_STATES = 1;
+parameter RAM_READ_WAIT_STATES_BITS = $clog2(RAM_READ_WAIT_STATES+1);
