6a3f56d63b73a206731afa94a13381d542a16b93
Luca Ceragioli <ceragioli_luca@libero.it>
2023-04-13 04:31:42 +0200

    Set interal frequency to 100 MHz from 200 MHz

--- design_files/globals.v
+++ design_files/globals.v
@@ -3,18 +3,18 @@
 `define GLOBALS_INCLUDED
 
 /* MAIN FREQ FROM PLL */
-`define MAIN_CLK_FREQ_MHZ       200
+`define MAIN_CLK_FREQ_MHZ       100
 
 /* SDCARD SPI PRESCALER SELECTION */
-`define SDCARD_MISSION_MODE_PSCLR   SPI_PSCLR_DIV32    /* Main clock is 200 MHz -> DIV8 = 25 MHz */
-`define STARTUP_PRESCALER           SPI_PSCLR_DIV1024 
+`define SDCARD_MISSION_MODE_PSCLR   SPI_PSCLR_DIV16
+`define STARTUP_PRESCALER           SPI_PSCLR_DIV512 
 
 /* I2C MASTER FREQ */
 `define I2C_IF_FREQ_KHZ         20
 
 /* Codec Main Clock (MCLK) generation */
 `define CODEC_MCLK_FREQ_HZ      16_934_400
-`define CODEC_MCLK_DIV          12          /*  If main clock is 203.2 MHz -> 203.2/12 = 12.933*/
+`define CODEC_MCLK_DIV          6          /*  If main clock is 203.2/2 MHz -> 203.2/2/6 = 12.933*/
 `define CODEC_FSAMPL_HZ         44_100
 
 /* Uncomment ignored config */
