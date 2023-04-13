8a2a0f7f8d453a4ee4d07ca5e8ba8e44dfbf9d5c
Luca Ceragioli <ceragioli_luca@libero.it>
2023-04-13 13:41:28 +0200

    Fixed error in costant widht

--- design_files/FAT32_reader/FAT32_reader.v
+++ design_files/FAT32_reader/FAT32_reader.v
@@ -142,7 +142,7 @@ assign fat32_data_region_start = fat32_fat_region_start + (fat32_sectors_per_fat
 
 assign dir_entry_idx_max = fat32_bytes_per_sector << (highest_bit(fat32_sectors_per_cluster) - DIR_ENTRY_SHIFT);
 
-assign file_good = (file_name_ch0 != 16'hE5) &&
+assign file_good = (file_name_ch0 != 8'hE5) &&
                    (file_attributes[FILE_ATTR_SUBDIRECTORY_BIT] == 0) && 
                    (file_ext[0] == "W") && (file_ext[1] == "A") && (file_ext[2] == "V");
                    
