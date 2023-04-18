5bb0bedb15bc1a980a84db20eae4edeaace56643
Luca Ceragioli <ceragioli_luca@libero.it>
2023-04-18 17:21:55 +0200

    Updated channels=2

--- convert_audio_file.sh
+++ convert_audio_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Usage: ./convert_audio_file.sh ./input_file.mp3
+# Usage: ./convert_audio_file.sh ./input_file.mp3 ./output_file.wav
 
-CHANNELS=1
+CHANNELS=2
 SAMPLING_RATE=44100
-ffmpeg -i $1 -flags +bitexact -map_metadata -1 -ar $SAMPLING_RATE -ac $CHANNELS output.wav
+ffmpeg -i $1 -flags +bitexact -map_metadata -1 -ar $SAMPLING_RATE -ac $CHANNELS $2
